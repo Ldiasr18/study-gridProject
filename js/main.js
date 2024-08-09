bfb5d1c982cbc2fd50a93351c615dcfd69f2e10a
Ldiasr18 <ldiasr18@gmail.com>
2024-08-09 19:00:25 -0300

    added user input for the grid, fixed README, added the hover effect

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
-const container = document.querySelector(".container");
 document.body.style.margin = "1px"
+
+const container = document.querySelector(".container");
 container.style.cssText = "display: flex; flex-wrap: wrap; gap: 2px; width: 960px; height: 960px;"
+const submitBtn = document.getElementById("submitBtn");
 
 
 
-for(let i = 0; i <= 6; i++){
-    // Create row
-    let row = document.createElement("div");
-    row.className = "column";
-    row.style.cssText = "display: flex; gap: 1px; flex-direction: column; flex: 1 0 0; box-sizing: border-box;";
-
-    for(let j = 0; j <= 6; j++){
+function createGrid(numberColumns, numberRows) {
+    container.innerHTML = '';
+    for (let i = 0; i < numberColumns; i++) {
+        // Create column
         let column = document.createElement("div");
-        column.className = "row";
-        column.style.cssText = "border: 1px solid black; background-color: white; box-sizing: border-box; flex: 1 0 0; aspect-ratio: 1/1; background-color: green";
-        row.appendChild(column);
+        column.className = "column";
+        column.style.cssText = "display: flex; gap: 1px; flex-direction: column; flex: 1 0 0; box-sizing: border-box;";
+        //create row
+        for (let j = 0; j < numberRows; j++) {
+            let cell = document.createElement("div");
+            cell.className = "cell";
+            cell.style.cssText = "border: 1px solid black; box-sizing: border-box; flex: 1 0 0; aspect-ratio: 1/1; background-color: green";
+            cell.addEventListener("mouseover", ()=>{
+                cell.style.backgroundColor = "black";
+            })
+            column.appendChild(cell);
+        }
+        container.appendChild(column);
     }
-    container.appendChild(row);
 }
+
+//maybe just let the user enter one value and make it square always
+submitBtn.addEventListener("click", ()=>{
+        const numberColumns = parseInt(document.getElementById("columnNumber").value)
+        const numberRows = parseInt(document.getElementById("rowNumber").value)
+        if(numberColumns < 1 || numberColumns > 100 || isNaN(numberColumns)){
+            alert("Number of columns must be between 1 and 100.");
+            return;
+        }
+        if(numberRows < 1 || numberRows > 100 || isNaN(numberRows)){
+            alert("Number of rows must be between 1 and 100.")
+            return;
+        }
+        createGrid(numberColumns, numberRows);
+    })
