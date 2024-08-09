bc2f0231c1e29121b4acc15c74ae1586ebff5723
Ldiasr18 <ldiasr18@gmail.com>
2024-08-09 19:27:57 -0300

    changed the colors

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-document.body.style.margin = "1px"
-
+document.body.style.margin = "1px;"
+document.body.style.cssText="display: flex; flex-direction: column; justify-content: center; align-items: center; background-color: #a0a0a0"
 const container = document.querySelector(".container");
 container.style.cssText = "display: flex; flex-wrap: wrap; gap: 2px; width: 960px; height: 960px;"
 const submitBtn = document.getElementById("submitBtn");
@@ -17,9 +17,9 @@ function createGrid(numberColumns, numberRows) {
         for (let j = 0; j < numberRows; j++) {
             let cell = document.createElement("div");
             cell.className = "cell";
-            cell.style.cssText = "border: 1px solid black; box-sizing: border-box; flex: 1 0 0; aspect-ratio: 1/1; background-color: green";
+            cell.style.cssText = "border: 1px solid black; box-sizing: border-box; flex: 1 0 0; aspect-ratio: 1/1; background-color: #606060";
             cell.addEventListener("mouseover", ()=>{
-                cell.style.backgroundColor = "black";
+                cell.style.backgroundColor = "#303030";
             })
             column.appendChild(cell);
         }
