58060bbda93698cdde7aae7c4124d90e7b336df9
Ldiasr18 <ldiasr18@gmail.com>
2024-08-09 19:38:17 -0300

    formatted the code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 document.body.style.margin = "1px;"
-document.body.style.cssText="display: flex; flex-direction: column; justify-content: center; align-items: center; background-color: #a0a0a0"
+document.body.style.cssText = "display: flex; flex-direction: column; justify-content: center; align-items: center; background-color: #a0a0a0"
 const container = document.querySelector(".container");
 container.style.cssText = "display: flex; flex-wrap: wrap; gap: 2px; width: 960px; height: 960px;"
 const submitBtn = document.getElementById("submitBtn");
-
-
 
 function createGrid(numberColumns, numberRows) {
     container.innerHTML = '';
@@ -18,7 +16,7 @@ function createGrid(numberColumns, numberRows) {
             let cell = document.createElement("div");
             cell.className = "cell";
             cell.style.cssText = "border: 1px solid black; box-sizing: border-box; flex: 1 0 0; aspect-ratio: 1/1; background-color: #606060";
-            cell.addEventListener("mouseover", ()=>{
+            cell.addEventListener("mouseover", () => {
                 cell.style.backgroundColor = "#303030";
             })
             column.appendChild(cell);
@@ -27,17 +25,16 @@ function createGrid(numberColumns, numberRows) {
     }
 }
 
-//maybe just let the user enter one value and make it square always
-submitBtn.addEventListener("click", ()=>{
-        const numberColumns = parseInt(document.getElementById("columnNumber").value)
-        const numberRows = parseInt(document.getElementById("rowNumber").value)
-        if(numberColumns < 1 || numberColumns > 100 || isNaN(numberColumns)){
-            alert("Number of columns must be between 1 and 100.");
-            return;
-        }
-        if(numberRows < 1 || numberRows > 100 || isNaN(numberRows)){
-            alert("Number of rows must be between 1 and 100.")
-            return;
-        }
-        createGrid(numberColumns, numberRows);
-    })
+submitBtn.addEventListener("click", () => {
+    const numberColumns = parseInt(document.getElementById("columnNumber").value)
+    const numberRows = parseInt(document.getElementById("rowNumber").value)
+    if (numberColumns < 1 || numberColumns > 100 || isNaN(numberColumns)) {
+        alert("Number of columns must be between 1 and 100.");
+        return;
+    }
+    if (numberRows < 1 || numberRows > 100 || isNaN(numberRows)) {
+        alert("Number of rows must be between 1 and 100.")
+        return;
+    }
+    createGrid(numberColumns, numberRows);
+})
